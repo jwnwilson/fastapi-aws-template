c786f189a748791885daf8ff9aeec759837e733b
Noel Wilson <jwnwilson88@gmail.com>
2023-01-15 02:43:30 +0000

    Adding tests from new project

--- {{cookiecutter.project_slug}}/src/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from fastapi.testclient import TestClient
-
-
-@pytest.fixture
-def client():
-    from app.adapter.into.fastapi.main import app
-
-    return TestClient(app)
--- {{cookiecutter.project_slug}}/src/tests/functional/test_api.py
+++
@@ -1,5 +0,0 @@
-def test_api_root(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "{{cookiecutter.project_slug}} service"}
-
